66f7ede7446594f8daa41fa27edf9b752d96e4a8
trieule50 <trieule50@gmail.com>
2021-08-30 18:43:39 -0400

    Complete integration test

--- util.test.js
+++ util.test.js
@@ -1,4 +1,4 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should out name and age', () =>{
     const text = generateText('Max', 29)
@@ -9,3 +9,10 @@ test('should out name and age', () =>{
 //     const text = generateText("", null);
 //     expect(text).toBe(" (null years old)")
 // })
+
+
+test('should generate a valid test output', ()=>{
+    const text = checkAndGenerate('Max', 29);
+    expect(text).toBe('Max (29 years old)')
+})
+
